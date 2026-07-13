5247b554c1154fe81f666b23c9162eb789655d69
pxrfectxx14 <pxrfecttx14@gmail.com>
2026-07-13 22:04:40 +0300

    homework 5

---
+++ src/HW_05/hw_05.java
@@ -0,0 +1,16 @@
+package HW_05;
+
+public class hw_05 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int a = 1; a <= 6; a++) {
+            sum += a;
+            System.out.println(a + ") Num is " + a + ", " + "sum is " + sum);
+        }
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
+    }
+}
